3ec432a43164dede2f165755a0268b5415ebe136
AniaSin <pierrat.laurent@gmail.com>
2022-11-30 18:28:51 +0100

    Mobs equipment

--- Actors/Mobs/Mob.gd
+++ Actors/Mobs/Mob.gd
@@ -9,6 +9,9 @@ func _ready():
 	var ship = load(data["ship"]).instance()	
 	add_child(ship)
 	ship.equip_slot(1, GameInstance.ship_elements["Engine"])
+	ship.equip_slot(8, GameInstance.ship_elements["Battery"])
+	ship.equip_slot(6, GameInstance.ship_elements["Gun"])
+	ship.equip_slot(7, GameInstance.ship_elements["Gun"])
 	print(ship.get_parent())
 
 
